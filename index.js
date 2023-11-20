ebb1eb5e7d8bce72ac5a1ac064b90346ea924d42
Vivek <vb79798@gmail.com>
2023-11-20 13:56:17 +0530

    origin update maitilabs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ config();
 const app= express();
 
 app.use(cors({
-  origin: 'https://maitilabs.org',
+  origin: 'https://www.maitilabs.org',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Authorization,Content-Type', // Include 'Authorization'
   credentials: true,
